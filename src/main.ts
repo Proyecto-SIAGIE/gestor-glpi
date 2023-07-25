5d0d73af0cbde083303349ffad79cb52640f69bc
CARLOS EDUARDO IZARRA TICLLA <caizarra@minedu.gob.pe>
2023-07-25 10:37:16 -0500

    feat: Add configuration to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('API Documentation')
+    .setDescription('Documentación de API del Proyecto SIAGIE. API de GLPI.')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  
+  await app.listen(+process.env.PORT);
+  console.log(`Application is running on: ${await app.getUrl()}`)
 }
 bootstrap();
