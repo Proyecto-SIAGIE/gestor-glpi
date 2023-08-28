40bf65c550f8f376efc151c56f10746174606645
CARLOS EDUARDO IZARRA TICLLA <caizarra@minedu.gob.pe>
2023-08-28 09:36:17 -0500

    feat: Add neccesary files

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { DatabaseModule } from './common/database/database.module';
 import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { ApiTokenCheckMiddleware } from './common/middleware/apiTokenCheck.middleware';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 
 @Module({
@@ -12,6 +13,10 @@ import { ConfigModule } from '@nestjs/config';
         GlpiApiRestModule, 
         GlpiModule, 
     ConfigModule.forRoot({ isGlobal: true }),
+    ThrottlerModule.forRoot({
+      ttl: +process.env.THROTTLER_MAX_SECONDS,
+      limit: +process.env.THROTTLER_MAX_CONSULTS
+    }),
     DatabaseModule,],
   controllers: [],
   providers: [],
--- src/main.ts
+++ src/main.ts
@@ -4,23 +4,36 @@ import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { resourceToModel } from './utils/mapping/resourceToModel.mapping';
 import * as bodyParser from 'body-parser';
+import helmet from 'helmet';
 resourceToModel();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({ origin: process.env.CORS_ORIGIN_URL != undefined ? process.env.CORS_ORIGIN_URL : "*" });
   
   app.use(bodyParser.json());
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({
+    disableErrorMessages: false,
+    transform: true
+  }));
 
   app.setGlobalPrefix('api/v1');
+
+  app.use(helmet({
+    frameguard: { action: 'deny' }, //establece que si alguien intenta incorporar tu sitio en un marco o iframe, el navegador debe negarlo, lo que ayuda a prevenir ataques de clickjacking.
+    hidePoweredBy: true, //Esta configuración elimina o cambia el encabezado X-Powered-By en las respuestas HTTP
+    xssFilter: true, //Esta configuración activa el filtro de scripting en sitios cruzados (XSS) proporcionado por los navegadores.
+    referrerPolicy: { policy: 'same-origin' }, //Esta configuración controla qué información sobre el origen de la solicitud se incluye en la cabecera Referer de las solicitudes salientes. 
+    //strictTransportSecurity: { maxAge: 31536000, includeSubDomains: true } //HSTS es una política de seguridad que obliga a los navegadores a usar siempre HTTPS para comunicarse
+  }))
 
   const config = new DocumentBuilder()
     .setTitle('API Documentation')
     .setDescription('Documentación de API del Proyecto SIAGIE. API de GLPI.')
     .setVersion('1.0')
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   
